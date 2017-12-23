3e52d5340f17837522a9e4ece468ebfa7a2b1a44
TKquality <lumis.eterne.168@gmail.com>
2017-12-24 05:03:11 +0900

    change to use element-ui components

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,40 @@
 <template>
   <div id="app">
-    <el-button @click="changeStatus()">ClickMe</el-button>
-    <div>
-      <transition name="el-zoom-in-center">
-        <img v-show="showStatus" src="./assets/logo.png">
-      </transition>
-    </div>
+    <el-container>
+      <el-aside id="titles">
+        <el-menu :unique-opened="true">
+          <el-submenu v-for="item in jsonData" :key="item.series" :index="item.series">
+            <template slot="title">
+              {{ item.series }}
+            </template>
+            <el-menu-item-group>
+              <el-menu-item class="aside-entry" v-for="story in item.stories" :key="story.id" :index="story.id" @click="changeImgUrl(story.imgPath)">chapter{{ story.titleCount }}:{{ story.title }}</el-menu-item>
+            </el-menu-item-group>
+           </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-main>
+        <el-button @click="changeStatus()">ClickMe</el-button>
+        <div>
+          <transition name="el-zoom-in-center">
+            <img v-show="showStatus" :src="imgUrl">
+          </transition>
+        </div>
+      </el-main>
+    </el-container>
   </div>
 </template>
 
 <script>
+import titleList from './assets/private/witches/list-dev.json'
 
 export default {
   name: 'app',
   data() {
     return {
       showStatus: true,
+      jsonData: titleList,
+      imgUrl: '/src/assets/logo.png',
     }
   },
   components: {
@@ -23,12 +42,15 @@ export default {
   methods: {
     changeStatus: function () {
       this.showStatus = !this.showStatus;
+    },
+    changeImgUrl: function (url) {
+      this.imgUrl = url;
     }
   }
 }
 </script>
 
-<style>
+<style scope>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -36,5 +58,12 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#titles {
+  width: 300px;
+}
+.aside-entry {
+  font-size: 0.75rem;
+  text-align: left;
 }
 </style>
